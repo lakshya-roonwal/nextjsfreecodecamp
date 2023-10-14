879753ca65cb29fbca3a3ea6efac673303d0c2c9
lakshya-roonwal <erenpool7214@gmail.com>
2023-10-14 14:32:01 +0530

    first commit

---
+++ src/app/login/page.tsx
@@ -0,0 +1,9 @@
+const LoginPage = () => {
+  return (
+    <div className="flex">
+      <h1 className="text-center text-white text-2xl">LoginPage</h1>
+    </div>
+  );
+};
+
+export default LoginPage;
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,15 @@
+"use client"
+import Link from "next/link"
+import React from "react"
+import { useRouter } from "next/router"
+
+
+
+const SignupPage = () => {
+  return (
+    <div className="flex">
+      <h1 className="text-center text-white text-2xl">Sign Up</h1>
+    </div>  )
+}
+
+export default SignupPage
